58839d1310862bd8fea3aa19965e19c570a5a156
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-10-31 11:10:24 -0500

    Create a function for add to inventory in bamazomManager.js

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# bamazon-Nodejs-MySQL
+# Bamazon-Nodejs-MySQL
  Create an Amazon-like storefront with the MySQL and Node.js that will take in orders from customers and deplete stock from the store's inventory
+
+## How this App Works
+
+* This app utilized `MySQL` to create a product list that displays to users. 
+
+* When user wants to buy an item, the database updates the amount and shows the user the purchase order costs. 
+
+* If there isn't enough of the product left in stock, the database doesn't fulfill the order, and tells the user. 
+
+---
+## Technologies used
+* MySQL 
---
+++ bamazonManager.js
@@ -0,0 +1,232 @@
+require('dotenv').config();
+
+// Requiring packages
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const columnify = require('columnify');
+const c = require('ansi-colors');
+const boxen = require('boxen');
+
+// Initial variables
+let updatedInventory = [];
+let updateItemID, updateName, updateDepartmet, updatePrice, updateQty, stockQty
+
+// Connect to database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: process.env.DATABASE_SECRET,
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(boxen(c.inverse.yellow("\n Bamazon Manager View \n"), { padding: 1, margin: 1, borderStyle: 'round' }));
+    console.log();
+    // Running this application will first display all of the options the manager can do
+    managerMenuOptions();
+});
+
+function managerMenuOptions() {
+    // Use inquirer package to ask manager what they want to do
+    inquirer
+        .prompt([{
+            name: "action",
+            type: "list",
+            message: "Select a menu option:",
+            choices: ["View Products for Sale"
+                , "View Low Inventory"
+                , "Add to Inventory"
+                , "Add New Product"
+                , "Quit Bamazon Manager"]
+        }])
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View Products for Sale":
+                    readProductsForSale();
+                    break;
+                case "View Low Inventory":
+                    readLowInventory();
+                    break;
+                case "Add to Inventory":
+                    // code block
+                    addToInventory();
+                    break;
+                case "Add New Product":
+                    // code block
+                    break;
+                case "Quit Bamazon Manager":
+                    quitBamazonManager();
+                    break;
+            }
+        });
+};
+
+function quitBamazonManager() {
+    // Print total cost to the customer
+    console.log(c.yellow("\nExiting Bamazon Manager App...\nHave a wonderful day!\n\n "));
+
+    // End connections
+    connection.end();
+};
+
+
+function readProductsForSale() {
+    console.log(c.inverse.green("\n  View Products for Sale  \n"));
+    var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        // Using the columnify package, column headings are extracted from the keys in supplied objects.
+        var columns = columnify(res, {
+            minWidth: 14,
+            config: {
+                product_name: { maxWidth: 70 }
+            }
+        })
+        console.log(columns)
+        managerMenuOptions();
+    });
+};
+
+
+function readLowInventory() {
+    console.log(c.inverse.green("\n  View Low Inventory  \n"));
+    console.log(c.green("Showing products with inventory less than 50 \n"));
+    // Read all products with inventory less than 50
+    var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE stock_quantity < 50";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        // Using the columnify package, column headings are extracted from the keys in supplied objects.
+        var columns = columnify(res, {
+            minWidth: 14,
+            config: {
+                product_name: { maxWidth: 70 }
+            }
+        })
+        console.log(columns)
+        managerMenuOptions();
+    });
+};
+
+function addToInventory() {
+    console.log(c.inverse.green("\n  Add to Inventory  \n"));
+    console.log(c.green("Current inventory of all products for sale \n"));
+
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        // Using the columnify package, column headings are extracted from the keys in supplied objects.
+        var columns = columnify(res, {
+            minWidth: 14,
+            config: {
+                product_name: { maxWidth: 70 }
+            }
+        })
+        console.log(columns)
+
+        // Create an array of choice for inquirer.js
+        var productChoices = [];
+
+        for (var i = 0; i < res.length; i++) {
+            var productChoice = res[i].product_name.toString();
+            productChoices.push(productChoice);
+        }
+        // Ask the manager which product he/she wants to add inventory
+        inquirer
+            .prompt([{
+                name: "item",
+                type: "list",
+                message: "Which item would you like to add inventory to?",
+                choices: productChoices
+            }, {
+                name: "quantity",
+                type: "input",
+                message: "What quantity would you like to add?",
+            }])
+            .then(function (answer) {
+
+                // Find the selected product and add the quantity to inventory
+                // Identify the product the customer wants to buy
+                res.forEach(product => {
+                    if (product.product_name == answer.item) {
+
+                        // Refer the product info
+                        updateItemID = product.item_id;
+                        updateName = product.product_name;
+                        updateDepartmet = product.department_name;
+                        updatePrice = product.price;
+                        originalQty = parseInt(product.stock_quantity);
+                        updateQty = parseInt(answer.quantity);
+                        stockQty = parseInt(product.stock_quantity) + parseInt(updateQty);
+
+                        // Store this product to a changedInventory variable
+                        var changedInventory = {
+                            item_id: updateItemID
+                            , product_name: updateName
+                            , department_name: updateDepartmet
+                            , price: updatePrice
+                            , original_quantity: originalQty
+                            , update_quantity: updateQty
+                            , stock_quantity: stockQty
+                        }
+                        // Add to the updatedInventory array
+                        updatedInventory.push(changedInventory);
+
+                        // Update inventory on the SQL database to reflect the remaining quantity.
+                        updateInventory();
+
+                        // Print order details to the user
+                        console.log(c.italic.yellow("\nAdd to Inventory\n") +
+                            
+                            c.green(updateName) + "\n " +
+                            c.cyan("Item#: " + updateItemID) + "\n " +
+                            c.cyan("Quantity: " + c.white(originalQty) + c.yellow(" + " + updateQty)) + "\n\n " +
+                            c.italic.yellow("The inventory for " + updateName + " has been updated to " + stockQty)
+                            + "\n---------------------------------------------------------------------------------------------------------------------------------\n");
+
+                        
+                        managerMenuOptions();
+                    };
+
+
+
+                });
+            });
+
+
+    });
+}
+
+function updateInventory() {
+    // Update stock quantity in the database
+    var query = "UPDATE products SET ? WHERE  ?";
+    connection.query(query, [{ stock_quantity: stockQty }, { item_id: updateItemID }], function (err, res) {
+        if (err) throw err;
+    });
+}
+
+
+
+function readProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        // Using the columnify package, column headings are extracted from the keys in supplied objects.
+        var columns = columnify(res, {
+            minWidth: 14,
+            config: {
+                product_name: { maxWidth: 70 }
+            }
+        })
+        console.log(columns)
+    });
+}
